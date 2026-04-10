6da857845daf3af6249b53912fc85a13d5fc8bce
TheCandyFace <pbeshynskyy@gmail.com>
2026-04-10 18:54:44 -0400

    Ajouter les endings et polish quelques trucs

--- Assets/DecisionManager.cs
+++ Assets/DecisionManager.cs
@@ -7,6 +7,9 @@ public class DecisionManager : MonoBehaviour
     public int totalScore = 0;
     public TextMeshProUGUI scoreText;
     public GameObject decisionPanel; // Drag your UI Container here
+    public GameObject targetPositiveEnding;
+    public GameObject targetNeutralEnding;
+    public GameObject targetNegativeEnding;
 
     public void MakeDecision(int pointValue)
     {
@@ -22,5 +25,22 @@ public class DecisionManager : MonoBehaviour
         Debug.Log("Choice made. Points: " + pointValue);
     }
 
-
+    public void startRightEnding()
+    {
+        if (totalScore == 0)
+        {
+            if (targetNeutralEnding  != null)
+                targetNeutralEnding.SetActive(true);
+        }
+        else if (totalScore < 0)
+        {
+            if (targetNegativeEnding != null)
+                targetNegativeEnding.SetActive(true);
+        }
+        else if (totalScore > 0)
+        {
+            if (targetPositiveEnding != null)
+                targetPositiveEnding.SetActive(true);
+        }
+    }
 }
